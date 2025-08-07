f7334886db28b95866192d4eb3eabd138aea510f
maxhimmel <himmelmax@gmail.com>
2025-08-07 08:18:15 -0700

    compile error when using latest versions of ai with current version of rag component

---
+++ convex/ai.ts
@@ -0,0 +1,14 @@
+import { components } from "./_generated/api";
+import { RAG } from "@convex-dev/rag";
+import { openai } from "@ai-sdk/openai";
+
+const rag = new RAG(components.rag, {
+  textEmbeddingModel: openai.embedding("text-embedding-3-small"), // <-- This throws and error
+  embeddingDimension: 1536,
+});
+
+/** Error from above ^
+ * Type 'EmbeddingModelV2<string>' is not assignable to type 'EmbeddingModelV1<string>'.
+    Types of property 'specificationVersion' are incompatible.
+    Type '"v2"' is not assignable to type '"v1"'.
+ */
